5931c926d811496ed47eeeb0c096b364b9d07238
Conrad Johnston <conrad.s.johnston@googlemail.com>
2018-09-03 16:41:43 +0200

    Standard parser proof of concept

--- aiida_nwchem/parsers/standard.py
+++ aiida_nwchem/parsers/standard.py
@@ -10,6 +10,10 @@
 from aiida_nwchem.parsers import BasenwcParser
 from aiida_nwchem.calculations.standard import StandardCalculation
 from aiida.orm.data.parameter import ParameterData
+from aiida.orm.data.structure import StructureData
+import numpy as np
+import ase 
+import re
 
 class StandardParser(BasenwcParser):
     """
@@ -23,6 +27,9 @@ class StandardParser(BasenwcParser):
     - DFT
     - Geo-opt
 
+    Multiple tasks are possible so we must parse each one.
+    The output will be parameter data containing a list 
+    of dictionaries in the order of the tasks. 
     """
     def __init__(self,calc):
         """
@@ -42,69 +49,143 @@ class StandardParser(BasenwcParser):
         Extracts output nodes from the standard output and standard error
         files.
         """
-        from aiida.orm.data.array.trajectory import TrajectoryData
-        import re
 
         state = None
         step = None
         scale = None
         with open(output_path) as f:
             lines = [x.strip('\n') for x in f.readlines()]
-
-        result_dict = dict()
-        trajectory = None
+        
+        # All of the parsed data 
+        data = {}
+        # A list to hold the dictionaries for each calculation task
+        result_list = []
+        # Dictionary to hold the data parsed from each task
+        task_dict = {}
+    
+        # For geo opt, lists to hold atom symbols, positions and the cell
+        symbols = []
+        positions = []
+        cell = []
+    
+        # List to hold all output nodes 
+        output_nodes_list = []
+        
         for line in lines:
             
             # 1. Determine which NWChem module we are using
             # SCF (HF)
-            if state is None and re.match('^\s*NWChem SCF Module\s*$',line):
-                state = 'nwchem-scf-module'
-                continue
+            if re.match('^\s*NWChem SCF Module\s*$',line):
+                if state is None:
+                    state = 'nwchem-scf-module'
+                    task_dict = {'module':'scf'}
+                    continue
+                elif state == 'nwchem-geoopt':
+                    if not tast_dict:
+                        task_dict = {'module':'geoopt_scf'}
+                    continue
             # DFT
-            if state is None and re.match('^\s*NWChem DFT Module\s*$',line):
-                state = 'nwchem-dft-module'
-                continue
+            if re.match('^\s*NWChem DFT Module\s*$',line):
+                if state is None:
+                    state = 'nwchem-dft-module'
+                    task_dict = {'module':'dft'}
+                    continue
+                elif state == 'nwchem-geoopt':
+                    if not task_dict:
+                        task_dict = {'module':'geoopt_dft'}
+                    continue
+    
             # Geometry Optimisation
             if state is None and re.match('^\s*NWChem Geometry Optimization\s*$',line):
-                state = 'nwchem-geometry-optimisation'
-                trajectory = TrajectoryData()
+                state = 'nwchem-geoopt'
+                task_dict = {}
                 continue
+    
             
             # 2. Get the data from each module
             # SCF (HF)
-            if state == 'nwchem-scf-module' and re.match('^\s*Final RHF \s*results\s*$',line):
-                state = 'final-rhf-results'
+            if state == 'nwchem-scf-module' and re.match('^\s*Final [ROU]+HF\s*results\s*$',line):
+                state = 'final-hf-results'
+                print 'yes'
                 continue
             if re.match('^\s*\-*\s*$',line):
                 continue
-            if state == 'final-rhf-results':
+            if state == 'final-hf-results':
                 result = re.match('^\s*([^=]+?)\s*=\s*([\-\d\.]+)$',line)
                 if result:
                     key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).lower())
-                    result_dict[key] = result.group(2)
-                else:
-                    state = 'nwchem-scf-module'
+                    task_dict[key] = result.group(2)
+                    
             # DFT 
             # Note the search for the Total DFT energy. NWChem doesn't otherwise
             # announce that the results are being printed. 
             if state == 'nwchem-dft-module' and re.match('^\s*Total DFT energy', line):
                 state = 'final-dft-results'
+                continue
             if state == 'final-dft-results':
                 result = re.match('^\s*([^=]+?)\s*=\s*([\-\d\.]+)$',line)
                 if result:
                     key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).lower())
-                    result_dict[key] = result.group(2)
-                else: 
-                    state = 'nwchem-dft-module'
+                    task_dict[key] = result.group(2)
+    
             # Geometry Optimisation
-            if state == 'nwchem-geometry-optimisation' and re.match('^\s*Step\s+\d+\s*$',line):
-                result = re.match('^\s*Step\s+(\d+)\s*$',line)
-                step = result.group(1)
+            if state == 'nwchem-geoopt' and re.match('^\s*Optimization converged\s*$',line):
+                state = 'final-geoopt-results'
                 continue
-            if state == 'nwchem-geometry-optimisation' and \
-                re.match('^\s*Output coordinates in a.u.',line):
-                state = 'nwchem-geometry-optimisation-coordinates'
-                result = re.match('scale by \s(*[\-\d\.]+)',line)
-                scale = result.group(1)
+            # - Parse step and energy
+            if state == 'final-geoopt-results':
+                result = re.match('^@\s*([\d]+)\s*([\-\d\.]+)',line)
+                if result:
+                    task_dict['final_step'] = result.group(1)
+                    # Note, this energy may be unreliable - do another scf on the final structure
+                    task_dict['final_opt_energy'] = result.group(2)
+            # - Parse coords
+            if state == 'final-geoopt-results' and re.match('^\s*Output coordinates in angstroms\s*',line):
+                state = 'final-geoopt-coords'
                 continue
-        return [('parameters', ParameterData(dict=result_dict))]
+            if state == 'final-geoopt-coords' and re.match('^[A-z\s\.-]*$', line):
+                continue
+            elif state == 'final-geoopt-coords':
+                result = re.match('^\s*[\d]+\s*([a-zA-Z]+)\s*[\-\d]+\.[\d]+'\
+                                  '\s*([\-\d\.]+)\s*([\-\d\.]+)\s*([\-\d\.]+)', line)
+                if result:
+                    symbols.append(result.group(1))
+                    positions.append([result.group(2),result.group(3),result.group(4)])
+                else:
+                    state = 'final-geoopt-results'
+            # - Parse cell 
+            if state == 'final-geoopt-results' and re.match('^\s*lattice vectors in angstroms',line):
+                state = 'nwchem-geoopt-cell'
+                continue
+            if state == 'nwchem-geoopt-cell':
+                result = re.match('^\s*a[1-3]=<\s*([\d\.\d]+)\s*([\d\.\d]+)\s*([\d\.\d]+)', line) 
+                if result:
+                    cell.append([result.group(1),result.group(2),result.group(3)])
+                else:
+                    state = None
+            
+            # If see timings, we are exiting the module  
+            if re.match('^ Task  times  cpu:\s*([\d\.\d]+)s\s*wall:\s*([\d\.\d]+)s', line):
+                result = re.match('^ Task  times  cpu:\s*([\d\.\d]+)s\s*wall:\s*([\d\.\d]+)s', line)
+                task_dict['cpu_time'] = result.group(1)
+                task_dict['wall_time'] = result.group(2)
+                result_list.append(task_dict.copy())
+                state = None
+                
+        # 3. Create output nodes
+        # - ParameterData
+        output_nodes_list.append(('parameters', ParameterData(dict={'data':result_list})))
+    
+        # - StructureData
+        if positions:
+            postions = np.array(positions, np.float64)
+            if not cell:
+                atoms = ase.Atoms(symbols=symbols, positions=positions)
+            else:
+                cell = np.array(cell, np.float64)
+                atoms = ase.Atoms(symbols=symbols, positions=positions, cell=cell)
+        
+            output_nodes_list.append(('output_structure', StructureData(ase=atoms)))
+    
+        return output_nodes_list
+
