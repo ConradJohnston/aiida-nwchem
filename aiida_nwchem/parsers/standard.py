9ec18fcc347623bb723d00c71b91362c26e33049
Conrad Johnston <conrad.s.johnston@googlemail.com>
2018-09-19 15:14:02 +0200

    Refactored parser; added vibrational support

--- aiida_nwchem/parsers/standard.py
+++ aiida_nwchem/parsers/standard.py
@@ -32,6 +32,7 @@ class StandardParser(BasenwcParser):
     The output will be parameter data containing a list 
     of dictionaries in the order of the tasks. 
     """
+        
     def __init__(self,calc):
         """
         Initialize the instance of StandardParser
@@ -39,7 +40,7 @@ class StandardParser(BasenwcParser):
         # check for valid input
         self._check_calc_compatibility(calc)
         super(StandardParser, self).__init__(calc)
-
+        
     def _check_calc_compatibility(self,calc):
         from aiida.common.exceptions import ParsingError
         if not isinstance(calc,StandardCalculation):
@@ -59,20 +60,12 @@ class StandardParser(BasenwcParser):
         # List to hold all output nodes 
         output_nodes_list = []
 
-        # Supported task types - dict to map task names to functions
-        parse_task = {
-            'scf': parse_scf,
-            'dft': parse_dft,
-            'geoopt': parse_geoopt,
-            'freq': parse_freq,
-        }
-                               
         # Cut the data in to lists
-        task_types, task_lines_list = separate_tasks(all_lines)
+        task_types, task_lines_list = self.separate_tasks(all_lines)
 
         # Loop over tasks, parsing each one
         for task, task_lines in zip(task_types, task_lines_list):
-            result_dict, nodes = parse_task[task](task_lines)
+            result_dict, nodes = self.parse_task[task](self,task_lines)
             result_list.append(result_dict)
             output_nodes_list = output_nodes_list + nodes
         
@@ -82,7 +75,7 @@ class StandardParser(BasenwcParser):
         return output_nodes_list
  
 
-    def separate_tasks(lines):
+    def separate_tasks(self,all_lines):
         """
         Parse the different task types and their associated lines
 
@@ -127,7 +120,7 @@ class StandardParser(BasenwcParser):
 
         return task_types, task_lines_list
 
-    def parse_scf(lines):
+    def parse_scf(self,lines):
         """
         Parse a SCF(HF) task block
         
@@ -160,7 +153,7 @@ class StandardParser(BasenwcParser):
         return task_dict, nodes
 
 
-    def parse_dft(lines):
+    def parse_dft(self,lines):
         """
         Parse a DFT task block
         
@@ -194,7 +187,7 @@ class StandardParser(BasenwcParser):
         return task_dict, nodes
 
     
-    def parse_geoopt(lines):
+    def parse_geoopt(self,lines):
         """
         Parse a geometry optimisation task block
         
@@ -208,9 +201,9 @@ class StandardParser(BasenwcParser):
         state = None
         symbols = []
         positions = []
+        cell = []
         
         for line in lines:
-        
             if re.match('^\s*Optimization converged\s*$',line):
                 state = 'final-results'
                 continue
@@ -223,7 +216,7 @@ class StandardParser(BasenwcParser):
                     task_dict['final_opt_energy'] = result.group(2)
                     continue
                 # Parse coords
-                if re.match('^\s*Output coordinates in angstroms\s*',line):
+                if re.match('^\s*Output coordinates in angstroms',line):
                     state = 'final-coords'
                     continue
                 # Parse cell
@@ -231,30 +224,42 @@ class StandardParser(BasenwcParser):
                     state = 'final-cell'
                     continue
             if state == 'final-coords':
-                result = re.match('^\s*[\d]+\s*([a-zA-Z]+)\s*[\-\d]+\.[\d]+'\
-                                  '\s*([\-\d\.]+)\s*([\-\d\.]+)\s*([\-\d\.]+)', line)
+                result = re.match('^\s*[\d]+\s*([a-zA-Z]+)\s*[\-\d\.]+'
+                                  '\s*([\-\d\.]+)\s*([\-\d\.]+)\s*([\-\d\.]+)$', line)
                 if result:
                     symbols.append(result.group(1))
                     positions.append([result.group(2),result.group(3),result.group(4)])
+                    continue
                 else:
-                    state = 'final-results'
-                continue
+                    if re.match('^\s*lattice vectors in angstroms',line):
+                        state = 'final-cell'
+                        continue
+                    if re.match('^$|^[\sA-z\.-]+$', line):
+                        continue
+                    else:
+                        state = 'final-results'
+                        continue
             if state == 'final-cell':
-                result = re.match('^\s*a[1-3]=<\s*([\d\.\d]+)\s*([\d\.\d]+)\s*([\d\.\d]+)', line) 
-                if result:
-                    cell.append([result.group(1),result.group(2),result.group(3)])
-                else:
+                if re.match('^\s*reciprocal lattice vectors',line):
                     state = 'final-results'
+                    continue
+                else:
+                    result = re.match('^\s*a[1-3]=<\s*([\d\.\d]+)\s*([\d\.\d]+)\s*([\d\.\d]+)', line) 
+                    if result:
+                        cell.append([result.group(1),result.group(2),result.group(3)])
+                        continue
+                    else:
+                        continue
 
             if re.match('^ Task  times  cpu:', line):
                 result = re.match('^ Task  times  cpu:\s*([\d\.\d]+)s\s*wall:\s*([\d\.\d]+)s', line)
                 task_dict['cpu_time'] = result.group(1)
                 task_dict['wall_time'] = result.group(2)
                 break
-    
+   
         # Create StructureData node 
         if positions:
-            postions = np.array(positions, np.float64)
+            positions = np.array(positions, np.float64)
         if not cell:
             # If the cell is specified, ASE defaults to 0,0,0, which throws an AiiDA
             # error for cell with volume of 0. Here we arbitrarily set the cell. 
@@ -269,7 +274,7 @@ class StandardParser(BasenwcParser):
         return task_dict, nodes
 
 
-    def parse_freq(lines):
+    def parse_freq(self,lines):
         """
         Parse a frequency analysis task block
         
@@ -288,27 +293,27 @@ class StandardParser(BasenwcParser):
                 state = 'final-results'
                 continue
             if state == 'final-results':
-                result = re.match('^\s([A-z\s\(\)]+)\s+=\s*([\d\.]+)',line)
+                result = re.match('^\s([A-z\s\(\)\-]+)\s+=\s*([\d\.]+)',line)
                 if result:
-                    if result.group(1) == 'Total Entropy':
+                    if result.group(1).strip() == 'Total Entropy':
                         state = 'final-entropy'
                         task_dict['entropy'] = {}
-                        key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).lower())
+                        key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).strip().lower())
                         task_dict['entropy'][key] = result.group(2)
                         continue
                     elif result.group(1) == 'Cv (constant volume heat capacity)':
                         state = 'final-cv'
-                        task_dict['head_capacity'] = {}
-                        task_dict['heat_capacity']['total_cv'] = result.group(2)
+                        task_dict['heat_capacity'] = {}
+                        task_dict['heat_capacity']['total'] = result.group(2)
                         continue
                     else:
-                        key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).lower())
+                        key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).strip().lower())
                         task_dict[key] = result.group(2)
                         continue
                 # Derivative Dipole
                 if re.search('Projected Derivative Dipole',line):
-                    state = 'final-dipole'
-                    dipole_list = []
+                    state = 'final-freq-results-dipole'
+                    dipoles_list = []
                     frequencies = []
                     continue
                 # Infrared 
@@ -320,7 +325,7 @@ class StandardParser(BasenwcParser):
             if state == 'final-entropy':
                 result = re.match('^\s*-\s([A-z\s\(\)]+)\s+=\s*([\d\.]+)',line)
                 if result:
-                    key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).lower())
+                    key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).strip().lower())
                     task_dict['entropy'][key] = result.group(2)
                 else:
                     state = 'final-results'
@@ -329,7 +334,7 @@ class StandardParser(BasenwcParser):
             if state == 'final-cv':
                 result = re.match('^\s*-\s([A-z\s\(\)]+)\s*=\s*([\d\.]+)',line)
                 if result:
-                    key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).lower())
+                    key = re.sub('[^a-zA-Z0-9]+', '_', result.group(1).strip().lower())
                     task_dict['heat_capacity'][key] = result.group(2)
                 else:
                     state = 'final-results'
@@ -337,17 +342,17 @@ class StandardParser(BasenwcParser):
             # Parse dipole data
             if state == 'final-freq-results-dipole':
                 result = re.match('^\s*[\d]\s*([\-\d\.]+)\s*\|\|'
-                                  's*([-\d.]+)\s*([-\d.]+)\s*([-\d.]+)$',line)
+                                  '\s*([-\d.]+)\s*([-\d.]+)\s*([-\d.]+)$',line)
                 if result:
                     # Get vibrational eigenvalues (cm^-1)
-                    frequencies.append(re.group(1))
+                    frequencies.append(result.group(1))
                     # Get dipole moments (cartesian, debye/angs)
-                    dipoles_list.append([re.group(2), re.group(3), re.group(4)])
+                    dipoles_list.append([result.group(2), result.group(3), result.group(4)])
                     continue
                 if re.match('^\s-+$', line):
                     state = 'final-results' 
                     task_dict['frequencies'] = frequencies
-                    task_dict['dipoles'] = np.array(dipoles_lisr, np.float64)
+                    task_dict['dipoles'] = np.array(dipoles_list, np.float64)
                     continue
             # Parse IR data 
             if state == 'final-freq-results-ir':
@@ -355,7 +360,7 @@ class StandardParser(BasenwcParser):
                                   '\s*([-\d.]+)\s*([-\d.]+)\s*([-\d.]+)$',line)
                 if result:
                     # Get intensity (arbitraty units)
-                    intensities.append(re.group(4))
+                    intensities.append(result.group(4))
                     continue
                 if re.match('^\s-+$', line):
                     state = 'final-results' 
@@ -370,3 +375,11 @@ class StandardParser(BasenwcParser):
                 break
         
         return task_dict, nodes
+    
+    # Supported task types - dict to map task names to functions
+    parse_task = {
+        'scf': parse_scf,
+        'dft': parse_dft,
+        'geoopt': parse_geoopt,
+        'freq': parse_freq,
+    }
